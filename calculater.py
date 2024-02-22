0e3cef843e45ecf258ab4ad4b8bee98a857196d4
Maxamed Xabiib <maxamedmax37@gmail.com>
2024-02-22 23:07:47 +0300

    frist commit

---
+++ calculater.py
@@ -0,0 +1,7 @@
+first_number = float(input("enter your first numper: "))
+operator = input("chose your operator:")
+second_number = float(input("enter your second numper:"))
+if operator == "+":
+    print(first_number + second_number) 
+else:
+    print("invailed operator")   
